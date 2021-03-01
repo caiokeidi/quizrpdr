7aca05f60d381e75eabbcfc56545bb950dd4b0a4
CaioKeidi <caiokeidi@gmail.com>
2021-03-01 18:38:12 -0300

    feat: Logo and some style to sidebar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import './SideBar.css'
+import React from "react";
+import "./SideBar.css";
+import logo from '../../assets/imgs/logo_rpdr.png'
 
-function SideBar(){
-    return(
-        <section className='SideBar'></section>
-
-    )
+function SideBar() {
+  return (
+  <>
+    <section className="SideBar">
+      <div className='logo_area'>
+        <img className='logo_rpdr' src={logo} alt='Logo'></img>
+        <p className='sub_title'>QUIZ</p>
+      </div>
+        
+    </section>
+  </>
+  );
 }
 
 export default SideBar;

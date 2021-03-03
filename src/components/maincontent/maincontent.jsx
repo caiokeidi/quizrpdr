9983c33ef6e23e36f7d90d94129968f26cf48887
CaioKeidi <caiokeidi@gmail.com>
2021-03-02 21:01:04 -0300

    feat: MainContent inner divs

--- src/components/maincontent/maincontent.jsx
+++ src/components/maincontent/maincontent.jsx
@@ -4,7 +4,12 @@ import './MainContent.css';
 function MainContent(){
     return(
         <section className='MainContent'>
-            
+            <div className='MainBox'>
+                <div className='img_box'></div>
+                <div className='div_pergunta'></div>
+                <div className='div_respostas'></div>
+            </div>
+        
         </section>
         
     )

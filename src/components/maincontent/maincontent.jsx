abb2768bd58f25995bc71c3fb888421c04ef9c1d
CaioKeidi <caiokeidi@gmail.com>
2021-03-03 11:34:49 -0300

    feat: btm_respostas to MainContent

--- src/components/maincontent/maincontent.jsx
+++ src/components/maincontent/maincontent.jsx
@@ -7,12 +7,26 @@ function MainContent(){
             <div className='MainBox'>
                 <div className='img_box'></div>
                 <div className='div_pergunta'></div>
-                <div className='div_respostas'></div>
+                <div className='div_respostas'>
+                    <div className='div_respostas_linha'>
+                        <div onclick={resposta_escolhida} className='btm_respostas' id='1'></div>
+                        <div onclick={resposta_escolhida} className='btm_respostas' id='2'></div>
+                    </div>
+                    <div className='div_respostas_linha'>
+                        <div onclick={resposta_escolhida} className='btm_respostas' id='1'></div>
+                        <div onclick={resposta_escolhida} className='btm_respostas' id='2'></div>
+                    </div>
+                    
+                </div>
             </div>
         
         </section>
         
     )
+}
+
+function resposta_escolhida(btm){
+    console.log(btm.id)
 }
 
 export default MainContent;
